e9bc44c2e9819204d56cae6ba394a353e8fdc371
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-10-29 19:49:12 -0400

    Updates

---
+++ precache-manifest.633f06c53bf74718cc6ad81e4a264941.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a2d35f9dec44f91380f6d044ce74b4",
+    "url": "/tetrisv1/index.html"
+  },
+  {
+    "revision": "5179bcf7c9a7b30aad84",
+    "url": "/tetrisv1/static/css/main.0bac23e7.chunk.css"
+  },
+  {
+    "revision": "8856fa953074747be7d9",
+    "url": "/tetrisv1/static/js/2.189d9ee1.chunk.js"
+  },
+  {
+    "revision": "5179bcf7c9a7b30aad84",
+    "url": "/tetrisv1/static/js/main.7c5e81e0.chunk.js"
+  },
+  {
+    "revision": "566de4ead718ff51fa30",
+    "url": "/tetrisv1/static/js/runtime-main.4c31675b.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetrisv1/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetrisv1/static/media/bg.2810fe6b.png"
+  }
+]);
